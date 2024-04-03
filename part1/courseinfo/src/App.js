6f513b2ea2f0c0856c74333b42205fb13d44ad84
Federico Verón <fedeva96@gmail.com>
2024-04-02 22:07:02 -0400

    Exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,19 +1,21 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const partss = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = ({ course }) => <h1>{course}</h1>;
   const Content = ({ parts}) => (
@@ -33,9 +35,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={partss}/>
-      <Total parts={partss} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   );
 };
