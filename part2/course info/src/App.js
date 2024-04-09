2462b58e4bed7cf889e331d13c84a48c49618c53
Federico Verón <fedeva96@gmail.com>
2024-04-09 16:00:04 -0400

    Exercise 2.3

--- part2/course info/src/App.js
+++ part2/course info/src/App.js
@@ -4,10 +4,10 @@ const Course = ({ course }) => (
     <p>{course.parts[0].name}: {course.parts[0].exercises}</p>
     <p>{course.parts[1].name}: {course.parts[1].exercises}</p>
     <p>{course.parts[2].name}: {course.parts[2].exercises}</p>
-    <strong>Total of {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} exercises</strong>
+    <strong>Total of {total(course.parts)} exercises</strong>
   </div>
 );
-
+const total = (parts) => parts.reduce((sum, part) => sum + part.exercises, 0);
 const App = () => {
   const course = {
     id: 1,
