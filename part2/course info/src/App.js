98d47428405ed492cc78d5f7b6821ad1f23d28b6
Federico Verón <fedeva96@gmail.com>
2024-04-09 15:56:24 -0400

    Exercise 2.2

--- part2/course info/src/App.js
+++ part2/course info/src/App.js
@@ -4,6 +4,7 @@ const Course = ({ course }) => (
     <p>{course.parts[0].name}: {course.parts[0].exercises}</p>
     <p>{course.parts[1].name}: {course.parts[1].exercises}</p>
     <p>{course.parts[2].name}: {course.parts[2].exercises}</p>
+    <strong>Total of {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} exercises</strong>
   </div>
 );
 
