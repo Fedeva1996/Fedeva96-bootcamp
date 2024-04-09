bda15328fd8f03ce4c3bda82a5a8bc844c03a9ce
Federico Verón <fedeva96@gmail.com>
2024-04-09 15:55:41 -0400

    Exercise 2.1

--- pruebas/src/App.js
+++ pruebas/src/App.js
@@ -1,25 +1,16 @@
-import { useState } from "react";
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>  
-)
-
-const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
-    setValue(newValue)
-  }
+import {Note} from "./Note.js"
+const App = ({notes}) => {
 
   return (
     <div>
-      {value}
-      <Button onClick={setToValue(1000)} text={1000}/>
-      <Button onClick={setToValue(0)} text={0}/>
-      <Button onClick={setToValue(value + 1)} text={'Aumentar en 1'}/>
+      <h1>Notes</h1>
+      <ol>
+        {notes.map((note) => (
+          <Note key={note.id} content={note.content} important={note.important}/>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
 export default App;
