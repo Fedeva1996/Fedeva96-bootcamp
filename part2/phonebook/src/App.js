613a4dfc2478a245c588d0ce8065a2be56d8af0d
Federico Verón <fedeva96@gmail.com>
2024-04-10 12:16:27 -0400

    Exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,35 +1,41 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
   const handleChange = (event) => {
     setNewName(event.target.value);
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const personObject = {name: newName}
-    setPersons(persons.concat(personObject))
-  }
+    event.preventDefault();
+    const personObject = { name: newName };
+    if (persons.find((person) => person.name === newName)) {
+      alert(newName + " ya existe en la lista");
+      return;
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName("");
+    }
+  };
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input  onChange={handleChange}/>
+          name: <input onChange={handleChange} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p>{person.name}</p>)}
+      {persons.map((person) => (
+        <p>{person.name}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
