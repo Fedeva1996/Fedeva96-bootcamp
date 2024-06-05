9d436f72a1fd07e4fc4ff6d099e4ad00dca61cf3
Federico Verón <fedeva96@gmail.com>
2024-06-05 10:19:28 -0400

    Excecise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://nodedb:${password}@cluster0.eycni4r.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (name == undefined || number == undefined) {
+  console.log("Phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} number: ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (name != undefined && number != undefined) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+  person.save().then((result) => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
